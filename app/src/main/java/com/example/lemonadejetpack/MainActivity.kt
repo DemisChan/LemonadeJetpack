860bc2ea2bf7c0dbabb93162c0c976df17d2052a
Demis Lavrentidis <demilavr@gmail.com>
2023-03-16 01:24:25 +0000

    created first Composable View

--- app/src/main/java/com/example/lemonadejetpack/MainActivity.kt
+++ app/src/main/java/com/example/lemonadejetpack/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.lemonadejetpack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lemonadejetpack.ui.theme.LemonadeJetpackTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    LemonTree()
                 }
             }
         }
@@ -30,14 +37,57 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun LemonTree(modifier: Modifier = Modifier) {
+    var clicks by remember { mutableStateOf(1) }
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        when (clicks) {
+            1 -> Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Text(text = stringResource(id = R.string.tap), fontSize = 18.sp)
+                Spacer(modifier = Modifier.height(16.dp))
+                Image(
+                    painter = painterResource(id = R.drawable.lemon_tree),
+                    contentDescription = "Tap the tree",
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .clickable { clicks++ })
+            }
+            2 -> Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Text(text = stringResource(id = R.string.tap), fontSize = 18.sp)
+                Spacer(modifier = Modifier.height(16.dp))
+                Image(
+                    painter = painterResource(id = R.drawable.lemon_squeeze),
+                    contentDescription = "Squeeze the lemon",
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .clickable { clicks++ })
+            }
+        }
+
+    }
+
+
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     LemonadeJetpackTheme {
-        Greeting("Android")
+        LemonTree(
+            Modifier
+                .fillMaxSize()
+                .wrapContentSize(Alignment.Center)
+        )
     }
 }
